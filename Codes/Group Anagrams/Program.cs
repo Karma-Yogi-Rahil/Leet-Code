4f6d3090d5e96fea39c87f3eb6818866df589019
Rahil Mehta <65954612+Karma-Yogi-Rahil@users.noreply.github.com>
2023-11-21 15:01:02 -0800

    Group Anangram

---
+++ Codes/Group Anagrams/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Group_Anagrams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, IList<string>> groupAnagramDict = new();
+
+            foreach (string word in strs)
+            {
+                string sortedString = new String(word.OrderBy(c => c).ToArray());
+
+                if (groupAnagramDict.ContainsKey(sortedString))
+                {
+                    groupAnagramDict[sortedString].Add(word);
+
+                }
+                else
+                {
+
+                    groupAnagramDict.Add(sortedString, new List<string>());
+                    groupAnagramDict[sortedString].Add(word);
+
+                }
+
+            }
+
+            IList<IList<string>> list =;
+
+           foreach(string word in groupAnagramDict.Keys)
+            {
+
+            }
+
+
+
+
+        }
+    }
+}
